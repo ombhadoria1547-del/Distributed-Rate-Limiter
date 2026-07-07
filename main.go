d80e0ddc8c1f6af2f4a19f4e36d237256067e505
Om Bhadoriya <ombhadoria1547@gmail.com>
2026-07-07 14:59:37 +0530

    feat: redis connectivity

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
 
 	router := gin.Default()
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	ctx := context.Background()
+
+	err := client.Ping(ctx).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Set(ctx, "username", "om", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	value, err := client.Get(ctx, "username").Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(value)
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
