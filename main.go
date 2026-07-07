1ea940f465bc5936e2054194b0066900a3657d26
Om Bhadoriya <ombhadoria1547@gmail.com>
2026-07-07 08:11:57 +0530

    Create first Gin server with ping endpoint

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func main(){
-	fmt.Println("Distributed Rate Limiter Project Started")
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(":8080")
 }
